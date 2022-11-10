abdc13ebffe5899f5cb58fda9d3c8b9f28d057db
Diego Bernabini <DIEGO.BERNABINI@local.ittcesena.it>
2022-11-10 09:24:08 +0100

    aggiunta della classe brano

---
+++ Wpf_esercizioGIT/Wpf_esercizioGIT/Brano.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_esercizioGIT
+{
+    internal class Brano
+    {
+        internal string Titolo { get; set; }
+        internal string Autore { get; set; }
+
+        internal double Durata { get; set; }
+
+        internal Brano()
+        {
+            Titolo = 
+                Autore =
+                Durata =
+        }
+    }
+}
